b42c399b5ff27b106f00ff006092ff09ceebe3ea
Flávio Dutra <flaviomdutra@outlook.com>
2024-01-20 22:54:41 -0300

    setup project

---
+++ src/entity/customer.ts
@@ -0,0 +1,47 @@
+import { Address } from "./address";
+
+export class Customer {
+  _id: string;
+  _name: string = "";
+  _address!: Address;
+  _active: boolean = true;
+
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate() {
+    if (this._address === undefined) {
+      throw new Error("Address is required");
+    }
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+
+  get Address(): Address {
+    return this._address;
+  }
+
+  set Address(address: Address) {
+    this._address = address;
+  }
+}
