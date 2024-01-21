6c6e3209a3964772134e651fae7aa8b4030e2d3c
Flávio Dutra <flaviomdutra@outlook.com>
2024-01-21 17:18:15 -0300

    Update package.json and main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { Customer } from "./entity/customer";
 import { Order } from "./entity/order";
 import { OrderItem } from "./entity/order-item";
 
-const customer = new Customer("1", "John Doe");
+const customer = new Customer("customer-1", "John Doe");
 const address = new Address("Main St", 123, "Anytown", "USA");
 
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
+const item1 = new OrderItem("1", "Item 1", 10, 'product-1', 1);
+const item2 = new OrderItem("2", "Item 2", 15, 'product-2', 2);
 
-const order = new Order("1", customer._id, [item1, item2]);
+const order = new Order("1", "customer-1", [item1, item2]);
